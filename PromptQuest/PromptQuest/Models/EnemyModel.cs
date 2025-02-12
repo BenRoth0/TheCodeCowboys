00f422377f7658002ef1d08bebf95d87b0560569
FrostyJoker02 <84209535+FrostyJoker02@users.noreply.github.com>
2025-02-11 23:31:49 -0800

    Progress on Default Enemy, functional but needs visual fixes

---
+++ PromptQuest/PromptQuest/Models/EnemyModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;   // Required for bitmaps
+
+namespace PromptQuest.Models
+{
+    public class EnemyModel
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
