ec580f933fe8918de730be27c3b429056156074a
fryguyfern <fryguyfern@gmail.com>
2025-02-18 22:21:51 -0800

    added static class to project titled currentplayer that hides the player from the url on character creation and creates a semi-persistant playermodel

---
+++ PromptQuest/PromptQuest/Models/CurrentPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace PromptQuest.Models
+{
+	public static class CurrentPlayer
+	{
+		public static PlayerModel _player = new PlayerModel { Name = "Test Player", MaxHealth = 10, CurrentHealth = 10 };
+
+		public static void SetPlayer(PlayerModel player) 
+		{
+			_player = player;
+		}
+		public static PlayerModel GetPlayer() 
+		{
+			return _player;
+		}
+	}
+}
