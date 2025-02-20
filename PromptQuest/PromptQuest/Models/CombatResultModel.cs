624ed46b53b78760a9e2d60debe1734cc894f21f
BenRoth0 <br.roths@gmail.com>
2025-02-20 02:10:46 -0800

    - Game engine moved from client side to server side. - Game state saved in a session - Page can now reload and does not change - Buttload of comments added everywhere - General code cleanup

---
+++ PromptQuest/PromptQuest/Models/CombatResultModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PromptQuest.Models {
+	public class CombatResult {
+		public string Message { get; set; } = "";
+		public int PlayerHealth { get; set; } = 0;
+		public int PlayerHealthPotions { get; set; } = 0;
+		public int EnemyHealth { get; set; } = 0;
+		public bool isPlayerDead { get; set; } = false;
+		public bool isEnemyDead { get; set; } = false;
+	}
+}
+
