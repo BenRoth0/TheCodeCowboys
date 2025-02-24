61ac94c2a86f5f40b25387ca0ede3ba7acfc4a16
Ben Roth <br.roths@gmail.com>
2025-02-24 00:54:17 -0800

    F86 - Implement Sessions (#47)

--- PromptQuest/PromptQuest/Controllers/HomeController.cs
+++ PromptQuest/PromptQuest/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using PromptQuest.Models;
+using PromptQuest.Services;
 using System.Diagnostics;
 
 namespace PromptQuest.Controllers {
 	public class HomeController:Controller {
 		private readonly ILogger<HomeController> _logger;
+		private readonly IGameService _gameService;
 
-		public HomeController(ILogger<HomeController> logger) {
+		public HomeController(ILogger<HomeController> logger, IGameService gameService) {
 			_logger = logger;
+			_gameService = gameService;
 		}
 
 		public IActionResult Index() {
+			// Fetch the game state for the current session.
+			GameState gameState = _gameService.GetGameState();
+			// Check if there's already a player character stored in the session.
+			if(gameState.Player != null) {
+				// They already have a character, bypass the main menu resume where they left off.
+				return RedirectToAction("Game","Game");
+			}
+			// They're a new player, show the main menu
 			return View();
 		}
 
