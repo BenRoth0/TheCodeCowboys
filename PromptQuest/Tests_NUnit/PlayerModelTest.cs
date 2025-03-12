9cd497877000dafdaddfc2f5caa62437187100bb
Kyle Blair <kblair21@mail.wou.edu>
2025-03-11 23:36:33 -0700

    Made a new migration for the Player Class and reformatted files I modified.

--- PromptQuest/Tests_NUnit/PlayerModelTest.cs
+++ PromptQuest/Tests_NUnit/PlayerModelTest.cs
@@ -43,6 +43,7 @@ namespace PlayerModelTest.Tests {
 		[TestCase("Warrior", true)]
 		[TestCase("Mage", true)]
 		[TestCase("Archer", true)]
+		[TestCase(null, false)]
 		public void PlayerModel_ClassValidation(string className, bool expectedIsValid) {
 			// Arrange
 			_player.Class = className;
