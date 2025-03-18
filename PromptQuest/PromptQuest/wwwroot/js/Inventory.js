82b9df8bb9e7dea7850324c7fb635c3cb709e239
Kyle Blair <kblair21@mail.wou.edu>
2025-03-17 20:05:45 -0700

    Modified the item display to be hidden until an item is clicked on. Also removes the items in the inventory when loading so it isn't duplicated.

--- PromptQuest/PromptQuest/wwwroot/js/Inventory.js
+++ PromptQuest/PromptQuest/wwwroot/js/Inventory.js
@@ -8,11 +8,19 @@ function Item(name, attack, defense, image) {
 }
 // This function will call the Item function to create new items and add them to the defaultItems array for testing
 function LoadItems() {
-	defaultItem1 = new Item("Helmet", 0, 2, "/images/PlaceholderItem1.png");
+	defaultItem1 = new Item("Jeweled Helmet", 0, 2, "/images/PlaceholderItem1.png");
 	defaultItem2 = new Item("Fiery Sword", 4, 0, "/images/PlaceholderItem2.png");
 	defaultItem3 = new Item("Frozen Shield", 1, 3, "/images/PlaceholderItem3.png");
 	defaultItem4 = new Item("Warded Sword", 3, 2, "/images/PlaceholderItem4.png");
 	defaultItems = [defaultItem1, defaultItem2, defaultItem3, defaultItem4];
+	// Clear existing items in inventory slots
+	for (let i = 1; i <= 20; i++) {
+		let slot = document.getElementById("inventory-slot-" + i);
+		while (slot.firstChild) {
+			slot.removeChild(slot.firstChild);
+		}
+	}
+	// Add items to inventory slots
 	for (i = 0; i < defaultItems.length; i++) {
 		item = defaultItems[i];
 		image = document.createElement("img");
@@ -33,4 +41,11 @@ function selectItem(item) {
 	document.getElementById("item-attack").innerHTML = item.attack;
 	document.getElementById("item-defense").innerHTML = item.defense;
 	document.getElementById("item-image").src = item.image;
+	// Show the elements
+	document.getElementById("item-name").style.display = "block";
+	document.getElementById("item-attack").style.display = "block";
+	document.getElementById("item-defense").style.display = "block";
+	document.getElementById("item-image").style.display = "block";
+	document.getElementById("shield-icon").style.display = "block";
+	document.getElementById("sword-icon").style.display = "block";
 }
