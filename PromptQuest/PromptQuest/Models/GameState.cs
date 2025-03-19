8d82d8fbbac7d767a56ddd3a081c73254554f3fb
fryguyfern <fryguyfern@gmail.com>
2025-03-19 10:15:38 -0700

    implemented f99

--- PromptQuest/PromptQuest/Models/GameState.cs
+++ PromptQuest/PromptQuest/Models/GameState.cs
@@ -39,6 +39,9 @@
 		public int PlayerLocation { get; set; } = 1;
 		///<summary> Whether or not the player has completed the current area </summary>
 		public bool IsLocationComplete{ get; set; } = false;
+
+		public int PlayerAdjustedATK { get; set; } = 0;
+		public int PlayerAdjustedDEF { get; set; } = 0;
 	}
 
 	/// <summary> Extension methods for the GameState model. </summary>
@@ -54,6 +57,8 @@
 			actionResult.IsPlayersTurn = gameState.IsPlayersTurn;
 			actionResult.PlayerLocation = gameState.PlayerLocation;
 			actionResult.IsLocationComplete = gameState.IsLocationComplete;
+			actionResult.PlayerAdjustedATK = gameState.Player.Attack + gameState.Player.item.ATK;
+			actionResult.PlayerAdjustedDEF = gameState.Player.Defense + gameState.Player.item.DEF;
 			return actionResult;
 		}
 	}
