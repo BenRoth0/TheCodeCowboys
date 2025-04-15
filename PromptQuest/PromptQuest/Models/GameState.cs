0a6b45fea6c0b9bd87857af53c22fb8355e19c22
Kyle Blair <kblair21@mail.wou.edu>
2025-04-15 15:36:55 -0700

    Made a display for the user's current floor on the map tab.

--- PromptQuest/PromptQuest/Models/GameState.cs
+++ PromptQuest/PromptQuest/Models/GameState.cs
@@ -19,6 +19,8 @@
 		public int PlayerLocation { get; set; } = 1;
 		///<summary> Whether or not the player has completed the current area </summary>
 		public bool IsLocationComplete{ get; set; } = false;
+		///<summary> The current floor the player is on. </summary>
+		public int Floor { get; set; } = 1;
 	}
 
 	/// <summary> A partial model of the GameStateModel returned to the view so that it can update what the action changed.  This way we don't have to return the entire GameStateModel. </summary>
