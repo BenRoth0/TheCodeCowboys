a41abdf345550a194789b2e3b0f0d7a2643a8869
fryguyfern <fryguyfern@gmail.com>
2025-04-28 18:38:03 -0700

    added BDD testing and cleaned up code to make tests pass

--- PromptQuest/PromptQuest/Services/CombatService.cs
+++ PromptQuest/PromptQuest/Services/CombatService.cs
@@ -4,7 +4,7 @@ namespace PromptQuest.Services {
 
 	public interface ICombatService {
 		void StartCombat(GameState gameState);
-		void PlayerAttack(GameState gameState, int attackMult = 1);
+		void PlayerAttack(GameState gameState, int attackMult = 1, bool decrementAbility=true);
 		void PlayerUseHealthPotion(GameState gameState);
 		void PlayerRest(GameState gameState);
 		void PlayerSkipRest(GameState gameState);
@@ -24,6 +24,7 @@ namespace PromptQuest.Services {
 		public void StartCombat(GameState gameState) {
 			gameState.InCombat = true;
 			gameState.IsPlayersTurn = true; // Player always goes first, for now.
+			gameState.Player.AbilityCooldown = 0; //reset player ability, may be removed down the line
 			if(gameState.PlayerLocation == 2 || gameState.PlayerLocation == 7) {
 				gameState.Enemy = GetElite(gameState);
 				gameState.AddMessage($"You have been attacked by the {gameState.Enemy.Name}!"); // Let the user know that combat started.
@@ -42,7 +43,7 @@ namespace PromptQuest.Services {
 		#region Player Action Methods
 
 		/// <summary> Calculates the damage that the player does to the enemy, updates the game state, then returns a message.</summary>
-		public void PlayerAttack(GameState gameState, int attackMult = 1) {
+		public void PlayerAttack(GameState gameState, int attackMult = 1, bool decrementAbility = true) {
 			// Get the player's equipped item
 			Item item = gameState.Player.ItemEquipped;
 			// Calculate damage as attack - defense.
@@ -52,6 +53,11 @@ namespace PromptQuest.Services {
 				damage = 1;
 			// Update enemy health.
 			gameState.Enemy.CurrentHealth -= damage;
+			//decrement ability cooldown if ability was not used
+			if (decrementAbility)
+			{
+				gameState.Player.AbilityCooldown -= 1;
+			}
 			// Return the result to the user.
 			gameState.AddMessage($"You attacked the {gameState.Enemy.Name} for {damage} damage");
 			// Check if enemy died.
@@ -78,8 +84,8 @@ namespace PromptQuest.Services {
 			switch (gameState.Player.Class.ToLower())
 			{
 				case "warrior"://attack for double power, uses the attack function
-					gameState.AddMessage($"You used your ability! You attacked the {gameState.Enemy.Name} for double damage!");
-					PlayerAttack(gameState, 2);
+					gameState.AddMessage($"You used your ability! You power up and perform a strong attack!");
+					PlayerAttack(gameState, 2, false);
 					gameState.Player.AbilityCooldown = 3;
 					break;
 				default:
--- PromptQuest/PromptQuest/wwwroot/js/Game.js
+++ PromptQuest/PromptQuest/wwwroot/js/Game.js
@@ -8,7 +8,7 @@ let campsiteButtonDisplay;
 let eventButtonDisplay;
 let treasureButtonDisplay;
 let dialogBox;
-let abilityCooldownIcon
+let abilityCooldownIcon;
 //Player action buttons
 let attackBtn;
 let healBtn;
@@ -131,7 +131,7 @@ function refreshPlayerDisplay() {
 	document.querySelectorAll(".player-defense").forEach(el => { el.textContent = gameState.player.defense + equippedItem?.defense ?? 0; });
 	document.querySelectorAll(".player-hp").forEach(el => { el.textContent = gameState.player.currentHealth + "/" + gameState.player.maxHealth + " HP"; });
 	document.getElementById("player-health-potions").textContent = gameState.player.healthPotions;
-	
+	abilityCooldownIcon.src = "/images/" +gameState.player.abilityCooldown + "_6_Clock.png"
 }
 
 function refreshEnemyDisplay() {
