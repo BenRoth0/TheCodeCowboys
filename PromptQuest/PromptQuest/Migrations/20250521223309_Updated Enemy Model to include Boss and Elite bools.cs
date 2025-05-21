6ca71a68fe6ddf24a5a1d0b6795e92a542602e95
Kyle Blair <kblair21@mail.wou.edu>
2025-05-21 15:33:55 -0700

    Added migration for the updated enemy model.

---
+++ PromptQuest/PromptQuest/Migrations/20250521223309_Updated Enemy Model to include Boss and Elite bools.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PromptQuest.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEnemyModeltoincludeBossandElitebools : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBoss",
+                table: "Enemies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsElite",
+                table: "Enemies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBoss",
+                table: "Enemies");
+
+            migrationBuilder.DropColumn(
+                name: "IsElite",
+                table: "Enemies");
+        }
+    }
+}
